e2dafb71438e5c4bf5212b269713687a70b164e9
hamsomp3 <janpolanco@javerianacali.edu.co>
2024-05-16 21:57:30 -0500

    new pie chart images with percentages for each category

--- notebooks/subjetividades.py
+++ notebooks/subjetividades.py
@@ -31,26 +31,26 @@ def create_data_directories(base_path):
 
         return list_of_folders#Pendiente
 
-def create_pie_chart(df, column_name, file_name, plot_title,output_folder,visualize):
-    unique_values = df[column_name].value_counts()
+# def create_pie_chart(df, column_name, file_name, plot_title,output_folder,visualize):
+#     unique_values = df[column_name].value_counts()
 
-    # Create a pie chart using Plotly Express
-    fig = px.pie(values=unique_values.values,
-                names=unique_values.index,
-                title=f'Pie Chart of {plot_title}',
-                labels={'names': '', 'values': ''},
-                hole=0.3
-                )
-    fig.update_traces(textinfo='percent+label',
-                    pull=[0.05]*len(unique_values.index)
-                    )
+#     # Create a pie chart using Plotly Express
+#     fig = px.pie(values=unique_values.values,
+#                 names=unique_values.index,
+#                 title=f'Pie Chart of {plot_title}',
+#                 labels={'names': '', 'values': ''},
+#                 hole=0.3
+#                 )
+#     fig.update_traces(textinfo='percent+label',
+#                     pull=[0.05]*len(unique_values.index)
+#                     )
     
-    if visualize:
-        fig.show()
+#     if visualize:
+#         fig.show()
     
 
-    # Save the plot as an HTML file
-    fig.write_html(output_folder.joinpath('pie_chart_'+file_name + '.html'))
+#     # Save the plot as an HTML file
+#     fig.write_html(output_folder.joinpath('pie_chart_'+file_name + '.html'))
 
 def create_bar_chart(df, column_name, file_name, plot_title,output_folder,visualize):
     unique_values = df[column_name].value_counts()
@@ -74,3 +74,36 @@ def create_bar_chart(df, column_name, file_name, plot_title,output_folder,visual
 
     # Save the plot as an HTML file
     fig.write_html(output_folder.joinpath('bar_chart_'+file_name + '.html'))
+
+def create_pie_chart(df, column_name, file_name, plot_title, output_folder, visualize):
+    # Obtener los valores únicos y sus recuentos
+    unique_values = df[column_name].value_counts()
+    
+    # # Convertir los recuentos a porcentajes
+    total_count = unique_values.sum()
+    unique_values = (unique_values / total_count) * 100
+    
+    # Crear el gráfico de pie usando Plotly Express
+    fig = px.pie(
+        values=unique_values.values,
+        names=unique_values.index,
+        title=f'Pie Chart of {plot_title}',
+        labels={'names': '', 'values': ''},
+        hole=0.3
+    )
+    
+    # Agregar etiquetas de porcentaje y ajustar la posición de las etiquetas
+    fig.update_traces(
+        textinfo='percent+label',
+        textfont_size=12,
+        texttemplate='%{value:.2f}%',
+        pull=[0.05] * len(unique_values.index),
+        marker=dict(line=dict(color='#000000', width=1))
+    )
+    
+    # Mostrar el gráfico si se especifica
+    if visualize:
+        fig.show()
+    
+    # Guardar el gráfico como archivo HTML
+    fig.write_html(output_folder.joinpath('pie_chart_' + file_name + '.html'))
